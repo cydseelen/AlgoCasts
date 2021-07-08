2025baf3650680f9c0cc05a6f47c06bf0d90c3e7
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-07-08 17:22:55 +0200

    Answered how to reverse a string with serveral solutions.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,58 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) =>
+//     character + reversed, '');
+// }
 
+// function reverse(str) {
+//   if (str === "")
+//     return "";
+//   else
+//     return reverse(str.substr(1)) + str.charAt(0);
+// }
+// console.log(reverse("hello"));
+
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     // ^ FOR Variable declaration (let) OF Iterable Object that we want to iterate through (str)
+//     reversed = character + reversed;
+//     // ^Adds each character to the reversed string
+//   }
+//   return reversed;
+// }
+// ^For of syntax
+
+// function reverse(str) {
+//   var newString = "";
+// // The for loop syntax below is not recommended, because you can easily make a mistake use "for of" instead
+//   for (var i = str.length - 1; i >= 0; i--) {
+//     newString += str[i];
+//   }
+//   return newString;
+// }
+// console.log(reverse("hello"));
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(reverse("cydnie"));
+
+function reverse(str) {
+  var splitString = str.split("");
+  // ^Split the argument string into an array of strings where each letter is a string
+  var reverseArray = splitString.reverse();
+  // ^This reverses the order of the array with the last letter at the beginning
+  var joinArray = splitString.join("");
+  // ^this joins the letter strings into 1 string with all the letters in the desired reverse order
+  return joinArray;
+  // ^Returns the reversed string
+}
+
+console.log(reverse("excellent"));
+// ^Prints the reversed string
 module.exports = reverse;
